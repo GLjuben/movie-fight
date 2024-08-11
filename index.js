6a5a0d90b1a2a25b0b9ab5332880d660f22c4312
Karnak <gorjanljuben@gmail.com>
2024-08-11 16:46:32 +0200

    added search input for movies

--- index.js
+++ index.js
@@ -27,14 +27,18 @@ autoCompleteRoot.innerHTML = `
 const searchInput = document.querySelector('.input');
 const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
+
 const onInput = async (event) => {
    const movies =  await fetchData(event.target.value)
+   resultsWrapper.innerHTML='';
    dropdown.classList.add('is-active');
-   for(let movie of movies) {
+
+   for (let movie of movies) {
         const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
         option.classList.add('dropdown-item');
         option.innerHTML = `
-        <img src="${movie.Poster}"/>
+        <img src="${imgSrc}"/>
         ${movie.Title}
         `
         resultsWrapper.appendChild(option)
