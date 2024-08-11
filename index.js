bcc84ca3a3f8cbcaa6991ee5f0c114a5caba8a37
Karnak <gorjanljuben@gmail.com>
2024-08-11 13:28:24 +0200

    added moive list

--- index.js
+++ index.js
@@ -5,26 +5,22 @@ const fetchData = async function(searchTerm) {
             s: searchTerm
         }
     });
-    console.log(response.data);
+    return response.data.Search;
 }
 
 const searchInput = document.querySelector('input')
 
-const debounce = (func,delay = 1000) => {
-    let timeoutId;
-    return (...args)=> {
-        if (timeoutId) {
-            clearTimeout(timeoutId)
-        }
-        
-        timeoutId = setTimeout(()=>{
-            func.apply(null,args)
-        },delay)
-    };
-};
 
-const onInput = (event) => {
-    fetchData(event.target.value)
+const onInput = async (event) => {
+   const movies =  await fetchData(event.target.value)
+   for(let movie of movies) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${movie.Poster}"/>
+        <h2>${movie.Title}</h2>
+        `
+        document.querySelector('#target').appendChild(div)
+   }
 }
 
 searchInput.addEventListener('input', debounce(onInput,750))
